8f8b1e6969bdb4c7ee63917ca60a45cf74479797
MedvedevDD <medvedev_dd@inbox.ru>
2021-11-17 22:52:38 +0300

    adding
    adding

---
+++ 01-first-project/my-app-1/src/Temp/4test.js
@@ -0,0 +1,18 @@
+function squareSum(numbers){
+    var sum = 0
+    for (var i = 0; i < numbers.length; i++)
+         var square = numbers[i] **2
+
+    sum += square;
+}
+
+function multiply(a, b){
+    c =  a * b
+    return c
+}
+
+function numberToString(num) {
+    string = String(num);
+    return string;
+    // Return a string of the number here!
+}
