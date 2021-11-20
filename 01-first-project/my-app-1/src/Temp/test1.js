8ec1d1800992191635bf6af2979724a0b09abbac
MedvedevDD <medvedev_dd@inbox.ru>
2021-11-20 19:24:00 +0300

    Update test1.js
    Update and refactor

--- 01-first-project/my-app-1/src/Temp/test1.js
+++ 01-first-project/my-app-1/src/Temp/test1.js
@@ -4,19 +4,20 @@ function golfScore(par, strokes) {
     // Only change code below this line
 // for (i = 0, i < )
     if (strokes === 1) {
-        return names[0];}
-    else if (strokes <= par - 2) {
-        return names[1];}
-    else if (strokes == par - 1) {
-        return names[2];}
-    else if (strokes == par) {
-        return names[3];}
-    else if (strokes == par + 1) {
-        return names[4];}
-    else if (strokes == par + 2) {
-        return names[5];}
-    else if (strokes >= par + 3) {
-        return names[6];}
+        return names[0];
+    } else if (strokes <= par - 2) {
+        return names[1];
+    } else if (strokes == par - 1) {
+        return names[2];
+    } else if (strokes == par) {
+        return names[3];
+    } else if (strokes == par + 1) {
+        return names[4];
+    } else if (strokes == par + 2) {
+        return names[5];
+    } else if (strokes >= par + 3) {
+        return names[6];
+    }
     // Only change code above this line
 }
 
@@ -34,3 +35,36 @@ var summation = function (num) {
     // Code here
 }
 summation(10);
+
+function makeNegative(num) {
+
+    var negative = (num > 0 ? num * -1 : num)
+
+    return negative;
+}
+
+makeNegative(-10)
+
+function findNeedle(haystack) {
+    var ind = haystack.indexOf("needle")
+
+    return "found the needle at position " + ind;
+
+}
+
+function basicOp(operation, value1, value2) {
+    switch (operation) {
+        case "+" :
+            return value1 + value2;
+
+        case "-" :
+            return value1 - value2;
+
+        case "*" :
+            return value1 * value2;
+
+        case "/" :
+            return value1 / value2;
+    }
+}
+basicOp("*", 5, 6)
