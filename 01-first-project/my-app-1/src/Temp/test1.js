f01416c2f537514b2e79af1a385e9cd8b58a256a
MedvedevDD <medvedev_dd@inbox.ru>
2021-11-22 00:12:57 +0300

    Update test1.js

--- 01-first-project/my-app-1/src/Temp/test1.js
+++ 01-first-project/my-app-1/src/Temp/test1.js
@@ -193,3 +193,26 @@ function simpleMultiplication(number) {
     }
     return result;
 }
+
+let count = 0;
+
+function cc(card) {
+    switch (card) {
+        case 10:
+        case 'J':
+        case 'Q':
+        case 'K':
+        case 'A':
+            count = count - 1;
+            break;
+        case 2:
+        case 3:
+        case 4:
+        case 5:
+        case 6:
+            count = count + 1;
+    }
+    return (count <= 0 ? count + ' Hold' : count + ' Bet');
+   }
+
+cc(2);cc(3);cc(7);cc('K');cc('A');
