92dc0e9a77a681d6ac8dd870fbbcf92b97cf5819
MedvedevDD <medvedev_dd@inbox.ru>
2021-11-25 10:56:42 +0300

    Update test1.js

--- 01-first-project/my-app-1/src/Temp/test1.js
+++ 01-first-project/my-app-1/src/Temp/test1.js
@@ -365,3 +365,16 @@ function pipeFix(numbers) {
     }
     return newArray;
 }
+
+function findLongest(str) {
+
+    var spl = str.split(" ");
+    var longest = 0
+
+    for (var i = 0; i < spl.length; i++) {
+        if (spl[i].length > longest) {
+            longest = spl[i].length
+        }
+    }
+    return longest
+}
